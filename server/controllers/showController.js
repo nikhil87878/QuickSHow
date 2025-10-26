c7f30ff7063136f77d6c4e7ad9e09cd31962b24f
nikhil87878 <nikhilsin2005@gmail.com>
2025-10-26 22:13:51 +0530

    ingeest

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import Movie from "../models/Movie.js";
 import Show from "../models/Show.js";
 import { err } from "inngest/types";
+import { inngest } from "../inngest/inngest.js";
 
 // API to get now playing movies from TMDB API
 export const getNowPlayingMovies = async (req, res)=>{
